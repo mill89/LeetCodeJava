55f6edec1b7347c0cf71e6c02639c392bdbee76f
Milan Nasrytdinov <milan.nasrytdinov@gmail.com>
2024-09-02 12:11:30 +0600

    sorting

---
+++ src/src/sorting/CountingSort_RadixSort.java
@@ -0,0 +1,29 @@
+package sorting;
+
+public class CountingSort_RadixSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] countingSort(int[] theArray, int maxValue) {
+        // Массив со "счётчиками" размером от 0 до максимального значения
+        int numCounts[] = new int[maxValue + 1];
+        // В соответствующей ячейке (индекс = значение) увеличиваем счётчик
+        for (int num : theArray) {
+            numCounts[num]++;
+        }
+        // Подготавливаем массив для отсортированного результата
+        int[] sortedArray = new int[theArray.length];
+        int currentSortedIndex = 0;
+        // идём по массиву со "счётчиками"
+        for (int n = 0; n < numCounts.length; n++) {
+            int count = numCounts[n];
+            // идём по количеству значений
+            for (int k = 0; k < count; k++) {
+                sortedArray[currentSortedIndex] = n;
+                currentSortedIndex++;
+            }
+        }
+        return sortedArray;
+    }
+}
---
+++ src/src/sorting/ShuttleSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class ShuttleSort {
+    public static void main(String[] args) {
+        int[] array = {10, 2, 10, 3, 1, 2, 5};
+        System.out.println(Arrays.toString(array));
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[i - 1]) {
+                swap(array, i, i - 1);
+                for (int z = i - 1; (z - 1) >= 0; z--) {
+                    if (array[z] < array[z - 1]) {
+                        swap(array, z, z - 1);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void swap(int[] array, int ind1, int ind2) {
+        int tmp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = tmp;
+    }
+}
